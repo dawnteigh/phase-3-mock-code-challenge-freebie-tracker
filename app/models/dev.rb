603d76d84893484ba6360ad503e07a980c4091de
Donte Ladatto <donte279@gmail.com>
2022-06-08 17:42:01 -0500

    commit tree ef6bdca2fef2e077b400d1fc053e7011d01fe754

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        self.freebies.find_by(item_name: item_name) == nil ? false : true
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            freebie.dev = dev
+            freebie.save
+        else
+            "This freebie isn't mine to give, ask #{freebie.dev.name}."
+        end
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,20 @@ Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
 Company.create(name: "Dunder Mifflin", founding_year: 2002)
 Company.create(name: "Enron", founding_year: 1995)
+Company.create(name: "Nintendo", founding_year: 1889)
+Company.create(name: "DaleTech", founding_year: 1997)
+Company.create(name: "Krusty Krab", founding_year: 1999)
+Company.create(name: "Game Freak", founding_year: 1989)
 
 puts "Creating devs..."
 Dev.create(name: "Rick")
 Dev.create(name: "Morty")
 Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
+Dev.create(name: "Mr. Poopy Butthole")
+Dev.create(name: "Dale Gribble")
+Dev.create(name: "Michael Scott")
+Dev.create(name: "Squidward Tentacles")
 
 puts "Creating freebies..."
 
@@ -17,5 +25,13 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+32.times do
+Freebie.create(
+    item_name: ["Plumbus", "Toilet Plunger", "Little Chocolates", "Hatrack", "Mousepad that's actually a mouse", "Muffin basket", "Kangaroo Sticker Set", "Damp Business Card", "Rubber Chicken"].sample,
+    value: rand(1..1000),
+    dev_id: rand(1..8),
+    company_id: rand(1..8)
+)
+end
 
 puts "Seeding done!"
